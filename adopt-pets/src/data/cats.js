ab49db1e1555fbf88be3b1b95fa4c4c2ee349bdd
Rapha-Y <raph.yuki@gmail.com>
2020-07-21 13:46:12 -0300

    added bootstrap to display pet data

---
+++ adopt-pets/src/data/cats.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'Sebastian',
+    breed: 'tuxedo',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'england',
+    notes: 'Obedient, well-mannered and a little sinister.'
+  },
+  {
+    name: 'Jacuzzi',
+    breed: 'tabby',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weight: 17,
+    location: 'italy',
+    notes: 'Shy and emotional, but very friendly.'
+  },
+  {
+    name: 'Cat',
+    breed: 'tabby',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'gray',
+    weight: 15,
+    location: 'united states',
+    notes: 'Cuddly, likes warm spots.'
+  },
+  {
+    name: 'Yoruichi',
+    breed: 'bombay',
+    species: 'cat',
+    gender: 'female',
+    age: 0.9,
+    color: 'black',
+    weight: 9,
+    location: 'japan',
+    notes: 'Adventurous'
+  }
+]
---
+++ adopt-pets/src/data/dogs.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Akane',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'japan',
+    notes: 'Obedient and kind, very serious.'
+  },
+  {
+    name: 'Narancia',
+    breed: 'mutt',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'italy',
+    notes: 'Playful and slightly aggressive.'
+  },
+  {
+    name: 'Irina',
+    breed: 'afghan hound',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    location: 'japan',
+    notes: 'Temperamental, needy.'
+  },
+  {
+    name: 'Lux',
+    breed: 'golden retriever',
+    gender: 'female',
+    age: 2,
+    color: 'black',
+    weight: 44,
+    location: 'demacia',
+    notes: 'Bright and cheerful.'
+  }
+]

4488c3913016a3a4c8679c2648c4639560d89bd8
Rapha-Y <raph.yuki@gmail.com>
2020-10-25 00:03:53 -0300

    added data JSON Placeholder integration

--- vue_crash_todolist/src/App.vue
+++ vue_crash_todolist/src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -20,32 +21,29 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "First todo",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Second todo",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Third todo",
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id != id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(() => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
   }
 }
 </script>
